3dd9d74bf67b2e2c570c1389b92b5aabe69f9581
Nike5170 <milevskijj.andrejj@gmail.com>
2019-12-17 22:49:42 +0200

    tab changes

--- js/main.js
+++ js/main.js
@@ -14,3 +14,19 @@ function go(id) {
         if (deg == 0) clearInterval(id);
     };
 };
+// tab changes
+
+function openTab(evt, tab) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("nav-item");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" acti", "");
+    }
+    document.getElementById(tab).style.display = "block";
+    evt.currentTarget.className += " acti ";
+    evt.currentTarget.classList.add(" acti ");
+}
